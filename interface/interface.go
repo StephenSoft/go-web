2ee9f76d4c52046f7406ca98bba301d714c09ab3
StephenZzz <stephenzhao94@163.com>
2022-08-03 13:05:48 +0800

    inter face 1

---
+++ interface/interface.go
@@ -0,0 +1,44 @@
+package main
+
+import(
+	"fmt"
+)
+
+type Sleeper interface{
+	sleep()
+}
+
+type User struct{
+	Name string
+	Age int
+	Gender int
+}
+
+func (user *User) sleep() (sleepIno string){
+	sleepIno = user.Name + "Sleepping Now"
+	// fmt.Println(sleepIno)
+	return
+}
+
+type Admin struct{
+	User
+	title string
+}
+
+func (admin *Admin) sleep()(sleepInfo string){
+	sleepInfo = admin.title + ":" + admin.Name + "Sleepping Now"
+	return
+}
+
+
+func main(){
+	user := User{"小明", 18, 1}
+	info := user.sleep()
+
+	admin := Admin{User: User{Name: "长贵", Age: 56, Gender: 1}, title: "象牙山村长" }
+
+	adminInfo := admin.sleep()
+	
+	fmt.Println(info)
+	fmt.Println(adminInfo)
+}
